e08722cbe64191f919a22508ab8900ad88f8b998
joinVazquez <emiliolouback@gmail.com>
2022-10-19 14:50:45 -0300

    LAB1

---
+++ menu.js
@@ -0,0 +1,15 @@
+document.write (
+
+"<li id='nav1' class='nav-item' style='padding-right: 1.5rem;'><a class='nav-link' </li>",
+"<li id='nav2' class='nav-item'><a class='nav-link active' href='index.html'>LOGIN</a></li>",
+"<li id='nav3' class='nav-item dropdown'><a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>Categorias </a><div class='dropdown-menu'><a class='dropdown-item' href='packs.html'>Packs</a>",
+"<a class='dropdown-item' href='mockups.html'>Mockups</a>",
+
+"<li id='nav8'>","<a class='dropdown-item' href='#' role='button' aria-haspopup='true' onclick='logout()'>SAIR</a>",
+"</div>"
+);
+
+
+/*"<li id='nav8' class='nav-item dropdown'><a class='nav-link dropdown-toggle' data-toggle='dropdown' href='#' role='button' aria-haspopup='true' aria-expanded='false'>PERFIL</a><div class='dropdown-menu'>",
+"<a class='dropdown-item' onclick='logout()'>SAIR</a>",
+"</div>"*/
